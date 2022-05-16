e50ec6448ed71f58d3fe83eae6aecaf5615284d0
jonasmachados <jonasmachado.ti@gmail.com>
2022-05-16 17:54:10 -0300

    Created Seller entities class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,19 @@
 
 package application;
-
+import java.util.Date;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
   
     public static void main(String[] args) {
         
-        Department department = new Department(1, "Deppartment");
+        Department department = new Department(1, "Book");
+        
+        Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, department);
 
-        System.out.println(department);
+        System.out.println(seller);
     }
     
 }
