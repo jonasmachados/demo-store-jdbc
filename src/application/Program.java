eb40d4c672ddfedc0b4a09b55082ec19c813122f
jonasmachados <jonasmachado.ti@gmail.com>
2022-05-16 17:37:28 -0300

    Department entities class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,21 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package application;
 
-/**
- *
- * @author Jonas
- */
+import model.entities.Department;
+
 public class Program {
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Department department = new Department(1, "Deppartment");
+
+        System.out.println(department);
     }
     
 }
---
+++ src/model/entities/Department.java
@@ -0,0 +1,71 @@
+package model.entities;
+
+import java.util.Objects;
+
+public class Department {
+
+    private static final long serialVersionUID = 1L;
+    
+    private Integer id;
+    private String name;
+
+    public Department() {
+    }
+
+    public Department(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Department " + "id: " + id + ", name: " + name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 31 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Department other = (Department) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+     
+
+}
